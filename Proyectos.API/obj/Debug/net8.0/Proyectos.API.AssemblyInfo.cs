254dc61fa875fcebfad22feb0d27e3e5a0ca201d
SAMUEL <SAMUEL@SARANGO>
2024-05-03 13:08:44 -0500

    MM

--- Proyectos.API/obj/Debug/net8.0/Proyectos.API.AssemblyInfo.cs
+++ Proyectos.API/obj/Debug/net8.0/Proyectos.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyectos.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca0e81341163b579b402fbc207ca066da8d8265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9d888605400d8335a08800e88e56952bba0d33")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyectos.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyectos.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
